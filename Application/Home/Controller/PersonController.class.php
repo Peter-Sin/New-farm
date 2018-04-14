f0947f93292e918cc799dfb13ffd4393d8ff7af0
conson <1908148708@qq.com>
2018-04-15 00:50:06 +0800

    更新推荐二维码

--- Application/Home/Controller/PersonController.class.php
+++ Application/Home/Controller/PersonController.class.php
@@ -57,27 +57,9 @@ class PersonController extends AllowController
             $object = new \QRcode();
             $errorCorrectionLevel = 'L';//容错级别
             $matrixPointSize = 6;//生成图片大小
-            $object->png($value, 'qrcode.png', $errorCorrectionLevel, $matrixPointSize, 2);
-            $logo = '';//准备好的logo图片
-            $QR = 'qrcode.png';//已经生成的原始二维码图
-            if ($logo !== FALSE) {
-                $QR = imagecreatefromstring(file_get_contents($QR));
-                $logo = imagecreatefromstring(file_get_contents($logo));
-                $QR_width = imagesx($QR);//二维码图片宽度
-                $QR_height = imagesy($QR);//二维码图片高度
-                $logo_width = imagesx($logo);//logo图片宽度
-                $logo_height = imagesy($logo);//logo图片高度
-                $logo_qr_width = $QR_width / 5;
-                $scale = $logo_width/$logo_qr_width;
-                $logo_qr_height = $logo_height/$scale;
-                $from_width = ($QR_width - $logo_qr_width) / 2;
-                //重新组合图片并调整大小
-                imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width,
-                    $logo_qr_height, $logo_width, $logo_height);
-            }
-            //输出图片
-            $filename=date("YmdHis").$info['vipid'].'.jpg';
-            imagepng($QR, './Public/image/recomcode/'.$filename);
+            $filename=time().$info['vipid'].'.jpg';
+            $url='./Public/image/recomcode/'.$filename;
+            $object->png($value, $url, $errorCorrectionLevel, $matrixPointSize, 2);
             $info['recomcode']=$filename;
             $data['recomcode']=$filename;
             $user->where("id='$uid'")->data($data)->save();
